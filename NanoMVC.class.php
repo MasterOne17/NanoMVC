1b89a6f2e3ebcdd60cf611cd4fffdba814adb3a6
MasterOne17 <masterone17@mailfish.de>
2012-05-26 00:24:02 +0200

    Created empty classes
    Created empty classes for the NanoMVC core, Controller, Model and View

--- NanoMVC.class.php
+++ NanoMVC.class.php
@@ -6,4 +6,10 @@
 	licensed under GPL v3
 */
 
+class NanoMVC {}
+
+class NanoMVC_Controller extends NanoMVC {}
+class NanoMVC_Model      extends NanoMVC {}
+class NanoMVC_View       extends NanoMVC {}
+
 ?>
